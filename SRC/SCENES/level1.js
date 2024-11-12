d63dfae6ee094c29815f73d5d3204e4f9227ef0d
Sofia <sofia.gelleni@mail.polimi.it>
2024-11-12 10:27:02 +0100

    initialisation of the game and the first structures

---
+++ SRC/SCENES/level1.js
@@ -0,0 +1,18 @@
+function preload(s) {
+
+}
+
+function create(s) {
+
+}
+
+
+function update(s) {
+
+}
+
+function destroy(s) {
+
+}
+
+PP.scenes.add("base", preload, create, update, destroy);
---
+++ SRC/main.js
@@ -0,0 +1,10 @@
+var config = {
+    canvas_width: 1280,
+    canvas_height: 720,
+    canvas_id: "game_area", // Specifica il div contenitore
+    background_color: 0x000055,
+    debug_mode: true, //quando si consegna il progetto da mettere false
+    gravity_value: 200
+};
+
+PP.game.create(config);
