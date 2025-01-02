397a06999911ade1b239431a3f3b34417af1f2ac
Gloria <gloria1.cattaneo@mail.polimi.it>
2025-01-02 21:16:17 +0100

    prova per web app, non funziona

---
+++ SRC/copertina.js
@@ -0,0 +1,9 @@
+function startGame() {
+    // Logica per avviare il gioco
+    console.log("Start Game clicked");
+}
+
+function showInstructions() {
+    // Logica per mostrare le istruzioni
+    console.log("Instructions clicked");
+}
