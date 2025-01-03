f02f1aa5240d2b32c8f8dde840ebcdca575b44cc
Sofia <sofia.gelleni@mail.polimi.it>
2025-01-03 12:01:21 +0100

    integrazione webapp

--- SRC/copertina.js
+++
@@ -1,9 +0,0 @@
-function startGame() {
-    // Logica per avviare il gioco
-    console.log("Start Game clicked");
-}
-
-function showInstructions() {
-    // Logica per mostrare le istruzioni
-    console.log("Instructions clicked");
-}
