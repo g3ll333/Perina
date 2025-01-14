2ebb0375ceddc75ac8e75533f1a669d8c1829798
Sofia <sofia.gelleni@mail.polimi.it>
2025-01-14 16:54:47 +0100

    aggiunta tavola vittoria finale

---
+++ SRC/SCENES/win.js
@@ -0,0 +1,40 @@
+let sfondo;
+let img_sfondo;
+let yesbutton;
+let homebutton;
+
+function preload(s) {
+    img_sfondo = PP.assets.image.load(s, "ASSETS/IMAGES/vincitafunghi.png");
+    yesbutton = PP.assets.image.load(s, "ASSETS/IMAGES/nextlevelvittoriafunghi.png");
+    homebutton = PP.assets.image.load(s, "ASSETS/IMAGES/homevincita.png");
+}
+
+function mouse_click_yes(s) {
+    PP.scenes.start("scene2");
+}
+
+function mouse_click_home(s) {
+    location.href = 'indexwebapp.html';
+}
+
+
+function create(s) {
+    sfondo = PP.assets.image.add(s, img_sfondo, 0, 0, 0, 0);
+
+    let buttonyes = PP.assets.image.add(s, yesbutton, 624, 450, 0.5, 0.5);
+    let home = PP.assets.image.add(s, homebutton, 105, 89.2, 0.5, 0.5);
+
+    PP.interactive.mouse.add(buttonyes, "pointerdown", mouse_click_yes);
+    PP.interactive.mouse.add(home, "pointerdown", mouse_click_home);
+    //da fare azione per pulsante no: far ritornare all'inizio della webapp
+}
+
+function update(s) {
+    // Eventuali aggiornamenti per la scena gameover possono essere aggiunti qui
+}
+
+function destroy(s) {
+    // Eventuali operazioni di pulizia per la scena gameover possono essere aggiunte qui
+}
+
+PP.scenes.add("win", preload, create, update, destroy);
--- SRC/enemy.js
+++ SRC/enemy.js
@@ -168,6 +168,6 @@ function update_enemy(s) {
 
     // Verifica se tutti i nemici sono stati uccisi 
     if (contatore_morti >= 3) {
-        PP.scenes.start("scene2");
+        PP.scenes.start("win");
     }
 }
