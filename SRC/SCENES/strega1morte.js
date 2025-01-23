e117e047a3401f83585b8f7239d5c634b7e311ea
Sofia <sofia.gelleni@mail.polimi.it>
2025-01-23 10:41:23 +0100

    aggiunto pere marce alla scena della morte della strega 1

--- SRC/SCENES/strega1morte.js
+++ SRC/SCENES/strega1morte.js
@@ -3,6 +3,17 @@ let floor_v;
 let img_background_1_v, img_background_2_v, img_background_3_v, img_background_4_v, img_background_5_v;
 let background_1_v, background_2_v, background_3_v, background_4_v, background_5_v;
 
+let img_peramarcia1_v;
+let img_peramarcia2_v;
+let img_peramarcia3_v;
+let img_peramarcia5_v;
+let img_peramarcia6_v;
+let peramarcia8_v;
+let peramarcia9_v;
+let peramarcia15_v;
+let peramarcia17_v;
+let peramarcia20_v;
+
 
 function preload(s) {
     console.log("Executing preload() - SCENE 1");
@@ -14,6 +25,11 @@ function preload(s) {
     img_background_4_v = PP.assets.image.load(s, "ASSETS/IMAGES/background4.png");
     img_background_5_v = PP.assets.image.load(s, "ASSETS/IMAGES/background5.png");
 
+    img_peramarcia1_v = PP.assets.image.load(s, "ASSETS/IMAGES/peramarcia1.png");
+    img_peramarcia2_v = PP.assets.image.load(s, "ASSETS/IMAGES/peramarcia2.png");
+    img_peramarcia3_v = PP.assets.image.load(s, "ASSETS/IMAGES/peramarcia3.png");
+    img_peramarcia5_v = PP.assets.image.load(s, "ASSETS/IMAGES/peramarcia5.png");
+    img_peramarcia6_v = PP.assets.image.load(s, "ASSETS/IMAGES/peramarcia6.png");
 
 
     preload_platforms_v(s);
@@ -40,6 +56,14 @@ function create(s) {
 
     background_5_v = PP.assets.tilesprite.add(s, img_background_5_v, 0, 10, 3840, 720, 0, 0);
     background_5_v.tile_geometry.scroll_factor_x = 0;
+
+    peramarcia8_v = PP.assets.image.add(s, img_peramarcia3_v, 2740, 120, 0, 1);
+    peramarcia9_v = PP.assets.image.add(s, img_peramarcia2_v, 3800, 575, 0, 1)
+    peramarcia15_v = PP.assets.image.add(s, img_peramarcia5_v, 2890, 575, 0, 1);
+    peramarcia17_v = PP.assets.image.add(s, img_peramarcia6_v, 3300, 300, 0, 1);
+    peramarcia20_v = PP.assets.image.add(s, img_peramarcia1_v, 3500, 575, 0, 1);
+
+
 
     // Creo un pavimento "trasparente"
     floor_v = PP.shapes.rectangle_add(s, 1920, 575, 4200, 1, "0x000000", 0);
